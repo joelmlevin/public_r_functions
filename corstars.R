aaf1df08d98c43c8b06424bf13c05f25cfc2f843
Joel Levin <jml176@pitt.edu>
2022-10-10 11:11:57 -0400

    add print stars option

--- corstars.R
+++ corstars.R
@@ -6,17 +6,23 @@
 # removeTriangle : remove upper or lower triangle
 # results :  if "html" or "latex"
 # the results will be displayed in html or latex format
-corstars <-function(x, method=c("pearson", "spearman"), removeTriangle=c("upper", "lower"),
-                    result=c("none", "html", "latex")){
+corstars <-function(x, 
+                    method=c("pearson", "spearman"), 
+                    removeTriangle=c("upper", "lower"),
+                    result=c("none", "html", "latex"),
+                    printstars = TRUE){
   #Compute correlation matrix
-  require(Hmisc)
+#  require(Hmisc)
+  
+  if(printstars == TRUE){print("****p<.001; ***p<.01, **p<.05; * p<.1")}
+  
   x <- as.matrix(x)
   correlation_matrix<-rcorr(x, type=method[1])
   R <- correlation_matrix$r # Matrix of correlation coeficients
   p <- correlation_matrix$P # Matrix of p-value 
   
   ## Define notions for significance levels; spacing is important.
-  mystars <- ifelse(p < .0001, "****", ifelse(p < .001, "*** ", ifelse(p < .01, "**  ", ifelse(p < .05, "*   ", "    "))))
+  mystars <- ifelse(p < .001, "**** ", ifelse(p < .01, "***  ", ifelse(p < .05, "**   ", ifelse(p < .1, "*   ", "    "))))
   
   ## trunctuate the correlation matrix to two decimal
   R <- format(round(cbind(rep(-1.11, ncol(x)), R), 2))[,-1]
@@ -49,8 +55,3 @@ corstars <-function(x, method=c("pearson", "spearman"), removeTriangle=c("upper"
     else print(xtable(Rnew), type="latex") 
   }
 } 
-
-print.corstars.stars <- function () {
-  print("p < .0001 = ****, p < .001 = ***, p < .01 = **, p < .05 = *")
-}
-  
